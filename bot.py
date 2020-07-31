ed7bbd50c32d53ee0979fb2f8de5c991f5987751
Sylouan Corfa <T4raal@gmail.com>
2020-07-31 21:47:24 +0200

    Added proper env and ported insults into a nice json file

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
+import json
 import os
 import random
+
+from dotenv import load_dotenv
 import discord
 
-TOKEN = "NzM4Njk4NjE0NjQxMTMxNTYx.XyPssg.sYKwA4s7NBjPwHuTT7zVlvsHt4Y"
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client()
 
 def create_insult():
-    first_row = ["Espèce de ", "Sale ", "Enfoiré de ", "Batard de ", " Vil ", "Putain de ", "Bordel de ", "Enculé de ", "Tête de ", "Salaud de ", "Foutu ", "Fichu ", "Satané ", "Diable de "]
-    second_row = ["nazi ", "con ", "débile ", "gauchiste ", "droitard ", "journalope ", "branleur ", "bouffon ", "casse-burnes ", "fripouille ", "fachiste ", "gland ", "garce ", "chieur ", "bite ", "boulet ", "connard ", "islamo-gauchiste ", "petite bite ", "pignouf ", "lopette ", "mange-merde ", "merdeux ", "minus ", "morue ", "moins-que-rien ", "naze ", "petite merde ", "porc ", "pute ", "raclure ", "résidu de capote ", "raclure de bidet ", "raté ", "sac à foutre ", "sac à merde ", "salope ", "sans-couilles ", "sent-la-pisse ", "teubé ", "tôcard ", "truie ", "trouduc ", "zgeg ", "vaurien "]   
-    third_row = ["de merde", "à la con", "trépané", "lobotomisé", "à poils longs", "vulgaire ", "de ses morts ", "de droite ", "de gauche ", "de ta race"]
-    return random.choice(first_row) + random.choice(second_row) + random.choice(third_row)
+    
+    data = json.load(open('insultes.json', "rb"), encoding="utf-8")
+
+    return random.choice(data['first_row']) + random.choice(data['second_row']) + random.choice(data['third_row'])
 
 @client.event
 async def on_message(message):
