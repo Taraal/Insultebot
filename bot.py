9d2ee8543379a13502726e5010536d10842700cd
Sylouan Corfa <T4raal@gmail.com>
2020-07-31 19:08:11 +0200

    Added more insults

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import os
 import random
 import discord
 
-TOKEN = "NzM4Njk4NjE0NjQxMTMxNTYx.XyPssg.PZfK90kvHGs2aUa9sCOUQxCCx6Y"
+TOKEN = "Votre token Discord"
 
 client = discord.Client()
 
 def create_insult():
-    first_row = ["Espèce de ", "Sale ", "Enfoiré de ", "Batard de ", " Vil "]
-    second_row = ["nazi ", "con ", "débile ", "gauchiste ", "droitard ", "journalope "]
-    third_row = ["de merde", "à la con", "trépané", "lobotomisé", "à poils longs"]
+    first_row = ["Espèce de ", "Sale ", "Enfoiré de ", "Batard de ", " Vil ", "Putain de ", "Bordel de ", "Enculé de ", "Tête de ", "Salaud de ", "Foutu ", "Fichu ", "Satané ", "Diable de "]
+    second_row = ["nazi ", "con ", "débile ", "gauchiste ", "droitard ", "journalope ", "branleur ", "bouffon ", "casse-burnes ", "fripouille ", "fachiste ", "gland ", "garce ", "chieur ", "bite ", "boulet ", "connard ", "islamo-gauchiste ", "petite bite ", "pignouf ", "lopette ", "mange-merde ", "merdeux ", "minus ", "morue ", "moins-que-rien ", "naze ", "petite merde ", "porc ", "pute ", "raclure ", "résidu de capote ", "raclure de bidet ", "raté ", "sac à foutre ", "sac à merde ", "salope ", "sans-couilles ", "sent-la-pisse ", "teubé ", "tôcard ", "truie ", "trouduc ", "zgeg ", "vaurien "]   
+    third_row = ["de merde", "à la con", "trépané", "lobotomisé", "à poils longs", "vulgaire ", "de ses morts ", "de droite ", "de gauche ", "de ta race"]
     return random.choice(first_row) + random.choice(second_row) + random.choice(third_row)
 
 @client.event
